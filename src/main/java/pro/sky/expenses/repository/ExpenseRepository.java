9c528e5dd076072c0b6f8b8c515ea9271752bef0
Awalone01 <sashaneiv01@yandex.ru>
2022-05-17 12:38:20 +0300

    Commit 1

--- src/main/java/pro/sky/expenses/repository/ExpenseRepository.java
+++ src/main/java/pro/sky/expenses/repository/ExpenseRepository.java
@@ -1,4 +1,11 @@
 package pro.sky.expenses.repository;
 
-public interface ExpenseRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pro.sky.expenses.entity.Expense;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
+    void delete(Integer id);
 }
--- src/main/java/pro/sky/expenses/service/ExpenseService.java
+++ src/main/java/pro/sky/expenses/service/ExpenseService.java
@@ -2,18 +2,28 @@ package pro.sky.expenses.service;
 
 import org.springframework.stereotype.Service;
 import pro.sky.expenses.entity.Expense;
+import pro.sky.expenses.repository.ExpenseRepository;
 
 import java.util.List;
 
 @Service
 public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
     public List<Expense> getAllExpenses() {
-        return List<Expense> ;
+        return expenseRepository.findAll();
     }
 
     public void createExpense(Expense expense) {
+        expenseRepository.save(expense);
     }
 
     public void deleteExpenseById(Integer id) {
+        expenseRepository.delete(id);
     }
 }
